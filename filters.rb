71c6f427316786a35985b4f347c9f283db2d5082
tajjav <taj.jav@hotmail.com>
2024-02-21 15:59:27 -0700

    add find, experienced, qualified_candidates and ordered_by_qualifications methods

--- filters.rb
+++ filters.rb
@@ -1,16 +1,37 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
-def find(id)
+  def find(id)
     # Your code Here
+    return candidate[:id]
   end
   
   def experienced?(candidate)
     # Your code Here
+    if candidate[:years_of_experience] > 2 do
+      return true
+    else
+      return false
+    end 
   end
   
   def qualified_candidates(candidates)
     # Your code Here
+    candidates.select { |candidate|
+      years_of_experience > 0
+      github_points >= 100
+      languages.include?("Ruby" || "Python")
+      Applied <= 15.days.ago.to_date
+      age > 17 
+    }
   end
   
   # More methods will go below
+  def ordered_by_qualifications(candidates)
+    candidates.sort {
+      years_of_experience
+      if !years_of_experience
+        github_points
+      end
+    }
+  end
